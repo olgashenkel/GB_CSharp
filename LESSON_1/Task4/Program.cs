b123d47bcaffdcbe49be20beba3feb023f0d29a2
Olga_Shenkel <olga_shenkel@mail.ru>
2024-03-07 20:10:50 +0200

    add LESSON_1/Task4

---
+++ LESSON_1/Task4/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача с пятью гирями.
+// Дано 5 гирь с разным весом в случайном порядке.
+// Найти вес самой тяжелой гири.
+
+int a = 36;
+int b = 7;
+int c = 28;
+int d = 4;
+int e = 15;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
