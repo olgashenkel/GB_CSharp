b1c9f97b889484732f92c8549e7aa01314dcfb9b
Olga_Shenkel <olga_shenkel@mail.ru>
2024-03-10 23:35:21 +0200

    add LESSON_2/Task2, LESSON_2/Task3, LESSON_2/Task4

--- LESSON_2/Task2/Program.cs
+++ LESSON_2/Task2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 5.1. Заполнение и вывод произвольного массива
+// Требуется заполнить массив целыми числами от 1 до N, 
+// где N – некоторое натуральное число и вывести его на экран.
+
+int n = 10;
+int[] arr = new int[n];
+int i = 0;
+
+while (i < arr.Length)
+{
+    arr[i] = i + 1;
+    // Console.Write(arr[i]);
+    // Console.Write(" ");
+    Console.Write($"{arr[i]} ");
+    i = i + 1;
+}
