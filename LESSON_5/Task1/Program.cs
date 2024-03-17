79d25fb1135780a111cc77f5626aa47614514f93
olga_shenkel <olga_shenkel@mail.ru>
2024-03-18 01:32:45 +0200

    add GB_CSharp

---
+++ LESSON_5/Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 1.
+// Вычислить факториал натурального числа N.
+
+int Fact(int n)
+{
+    if (n == 1 || n == 0)
+    {
+        Console.WriteLine($"Stop: {n}");
+        return 1;
+    }
+    Console.WriteLine(n);
+    return n * Fact(n - 1);
+}
+
+Console.WriteLine("Введите значение факторила:");
+int num = int.Parse(Console.ReadLine()!);
+
+
+Console.WriteLine(Fact(num));
+
