efe1277574f56c17263156c4ec822675fb39de8d
olga_shenkel <olga_shenkel@mail.ru>
2024-03-17 21:19:24 +0200

    add GB_CSharp

--- LESSON_practice-6/DZ/Program.cs
+++ LESSON_practice-6/DZ/Program.cs
@@ -111,8 +111,6 @@
 // РЕШЕНИЕ:
 // ***************
 
-// Задайте произвольную строку. Выясните, является ли она палиндромом.
-
 // char[] FromStringToArray(string input)
 // {
 //     char[] array = new char[input.Length];
@@ -162,63 +160,63 @@
 // РЕШЕНИЕ:
 // ***************
 
-string[] SplitString(string input)
-{
-    string[] answer = new string[CountString(input)];
-    int count = 0;
-    string result = "";
+// string[] SplitString(string input)
+// {
+//     string[] answer = new string[CountString(input)];
+//     int count = 0;
+//     string result = "";
 
-    for (int i = 0; i < input.Length; i++)
-    {
-        if (input[i] == ' ')
-        {
-            answer[count] = result;
-            result = "";
-            count++;
-        }
-        else
-        {
-            result += input[i];
-        }
-    }
-    answer[count] = result;
-    return answer;
-}
+//     for (int i = 0; i < input.Length; i++)
+//     {
+//         if (input[i] == ' ')
+//         {
+//             answer[count] = result;
+//             result = "";
+//             count++;
+//         }
+//         else
+//         {
+//             result += input[i];
+//         }
+//     }
+//     answer[count] = result;
+//     return answer;
+// }
 
-int CountString(string input)
-{
-    int count = 1;
+// int CountString(string input)
+// {
+//     int count = 1;
 
-    for (int i = 0; i < input.Length; i++)
-    {
-        if (input[i] == ' ')
-        {
-            count++;
-        }
-    }
+//     for (int i = 0; i < input.Length; i++)
+//     {
+//         if (input[i] == ' ')
+//         {
+//             count++;
+//         }
+//     }
 
-    return count;
-}
+//     return count;
+// }
 
-string[] RevertsSentence(string[] array)
-{
-    for (int i = 0; i < array.Length / 2; i++)
-    {
-        (array[array.Length - 1 - i], array[i]) = (array[i], array[array.Length - 1 - i]);
-    }
+// string[] RevertsSentence(string[] array)
+// {
+//     for (int i = 0; i < array.Length / 2; i++)
+//     {
+//         (array[array.Length - 1 - i], array[i]) = (array[i], array[array.Length - 1 - i]);
+//     }
 
-    return array;
-}
+//     return array;
+// }
 
-void PrintArray(string[] array)
-{
-    foreach (string item in array)
-    {
-        Console.Write($"{item} ");
-    }
-}
+// void PrintArray(string[] array)
+// {
+//     foreach (string item in array)
+//     {
+//         Console.Write($"{item} ");
+//     }
+// }
 
-Console.WriteLine("Введите предложение");
-string sentence = Console.ReadLine()!;
-Console.WriteLine("Первёрнутое предложение:");
-PrintArray(RevertsSentence(SplitString(sentence)));
+// Console.WriteLine("Введите предложение");
+// string sentence = Console.ReadLine()!;
+// Console.WriteLine("Первёрнутое предложение:");
+// PrintArray(RevertsSentence(SplitString(sentence)));
